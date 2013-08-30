0eeae70d4d83fd52229ee8d03e788718de60b8b6
MortenPlaypen <morten@playpenlabs.com>
2013-08-29 23:44:58 -0700

    Small text edit

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ var http = require("http");
 function onRequest(request, response) {
   console.log("Request received.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
+  response.write("Hello World - greetings from The Playpen");
   response.end();
 }
 
-http.createServer(onRequest).listen(process.env.PORT);
+http.createServer(onRequest).listen(8080); //Local server
+//http.createServer(onRequest).listen(process.env.PORT); //Cloud 9 server
 
 console.log("Server has started.");
